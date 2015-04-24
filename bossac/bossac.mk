e427c470bbf64de341371eac11cb837b058475f8
Martino Facchin <m.facchin@arduino.cc>
2015-04-24 15:20:50 +0200

    simplify patch and fix arch

--- bossac/bossac.mk
+++ bossac/bossac.mk
@@ -1,17 +1,15 @@
 ################################################################################
 #
-# avrdude
+# bossac
 #
 ################################################################################
 BOSSAC_VERSION = 2920b32899c5db4473b62db9fa2b14a9c224e4b4
 BOSSAC_SITE = $(call github,shumatech,BOSSA,$(BOSSAC_VERSION))
 BOSSAC_LICENSE = GPLv2+
 BOSSAC_LICENSE_FILES = LICENSE 
-# Sources coming from git, without generated configure and Makefile.in
-# files.
 
 define BOSSAC_BUILD_CMDS
-        $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) strip-bossac
+        $(MAKE) ARCH=$(ARCH) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D) strip-bossac
 endef
 
 define BOSSAC_INSTALL_TARGET_CMDS
